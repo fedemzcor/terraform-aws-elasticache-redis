12ed821e767a51fe359f8fa0df419910642ca3fc
Federico Márquez Córdova <fedemzcor@MacBook-Air-de-Federico.local>
2019-10-13 02:18:36 -0500

    dns name changed

--- main.tf
+++ main.tf
@@ -133,7 +133,7 @@ resource "aws_cloudwatch_metric_alarm" "cache_memory" {
 module "dns" {
   source  = "git::https://github.com/cloudposse/terraform-aws-route53-cluster-hostname.git?ref=tags/0.3.0"
   enabled = var.enabled && var.zone_id != "" ? true : false
-  name    = var.namespace
+  name    = "${var.namespace}-${var.name}"
   ttl     = 60
   zone_id = var.zone_id
   records = [join("", aws_elasticache_replication_group.default.*.primary_endpoint_address)]
